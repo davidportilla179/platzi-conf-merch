2a13bdb16d512f9b3066391b213c670fcf76797b
David Portilla <davidportilla179@hotmail.com>
2021-08-10 21:38:53 -0500

    class 22

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="Footer">
+        <p className="Footer-title">Platzi Conf Merch</p>
+        <p className="Footer-copy">Todos los izquierdos reservados</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
